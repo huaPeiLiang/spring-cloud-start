3ee7e71c1d4ffdb59fc93bf66b5359ee5391b95f
martin <martin@tenmaker.com>
2019-12-30 10:13:19 +0800

    eureka客户端搭建，基础模块hive

---
+++ hive/src/main/java/com/start/HiveApplication.java
@@ -0,0 +1,19 @@
+package com.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableAsync
+public class HiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiveApplication.class, args);
+    }
+
+}
