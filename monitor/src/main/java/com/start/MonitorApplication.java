e3abe2d0a184f7c1c174fafd14b2aa3f03439d21
martin <martin@tenmaker.com>
2020-01-25 19:25:36 +0800

    添加监控服务

--- monitor/src/main/java/com/start/MonitorApplication.java
+++ monitor/src/main/java/com/start/MonitorApplication.java
@@ -2,20 +2,20 @@ package com.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
+//@EnableEurekaClient
+//@EnableFeignClients
 @EnableAsync
 @EnableHystrixDashboard
-public class HiveApplication {
+public class MonitorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HiveApplication.class, args);
+        SpringApplication.run(MonitorApplication.class, args);
     }
 
 }
