e22cb8c95ae89996bfc94ac1111ed6f31a83a1d1
花培亮 <huapeiliang@wanmi.com>
2020-04-21 15:43:46 +0800

    添加websocket测试

---
+++ websocket/src/main/java/com/start/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.start.controller;
+
+import com.start.config.WebSocketServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import java.io.IOException;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("index")
+    public ResponseEntity<String> index(){
+        return ResponseEntity.ok("请求成功");
+    }
+
+    @GetMapping("page")
+    public ModelAndView page(){
+        return new ModelAndView("websocket");
+    }
+
+    @RequestMapping("/push/{toUserId}")
+    public ResponseEntity<String> pushToWeb(String message, @PathVariable String toUserId) throws IOException {
+        WebSocketServer.sendInfo(message,toUserId);
+        return ResponseEntity.ok("MSG SEND SUCCESS");
+    }
+}
